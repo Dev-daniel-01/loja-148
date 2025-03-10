bbdc4be0fd44a8d2c5a9527eba7d3ea3d37679c9
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-03-10 16:37:22 -0300

    Loja

---
+++ Login/script.js
@@ -0,0 +1,39 @@
+function login(){
+    document.getElementById("error-modal").style.display = "none"
+        document.getElementById("nome").style.border = "2px solid aliceblue"
+        document.getElementById("senha").style.border = "2px solid aliceblue"
+    var nome =$("#nome").val()
+    var senha = $("#senha").val()
+
+    // console.log(nome, senha)
+    if(nome && senha && nome === "admin" && senha === "1234"){
+        const user ={
+            name: nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000),
+        }
+
+        localStorage.setItem("usuario", JSON.stringify(user))
+
+        window.location.href = "../Loja"
+    }else{
+        document.getElementById("error-modal").style.display = "flex"
+        document.getElementById("nome").style.border = "2px solid red"
+        document.getElementById("senha").style.border = "2px solid red"
+          }     
+}
+
+function fecharError(){
+    document.getElementById("error-modal").style.display = "none"
+        document.getElementById("nome").style.border = "2px solid aliceblue"
+        document.getElementById("senha").style.border = "2px solid aliceblue"
+}
+
+function showPassword(){
+    var inputSenha= document.querySelector("#senha")
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
+    }else{
+        inputSenha.setAttribute("type", "password")
+    }
+}
